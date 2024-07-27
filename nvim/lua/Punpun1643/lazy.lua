4dfcbaa3b061a698fec6dff507b448233584bb9f
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-07-27 23:39:00 +0800

    feat: add more plugins

--- nvim/lua/Punpun1643/lazy.lua
+++ nvim/lua/Punpun1643/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("Punpun1643.plugins", {   
+require("lazy").setup({ { import = "Punpun1643.plugins" }, { import = "Punpun1643.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
